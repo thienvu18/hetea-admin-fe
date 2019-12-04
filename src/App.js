9b01a0cf720bacc563efb67c385f143c5f1d0806
vanvan1998 <nguyensivan1998@gmail.com>
2019-12-04 14:27:58 +0700

    add create admin page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 // import logo from './logo.svg';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
-import "./assets/vendor/nucleo/css/nucleo.css";
-import "./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
-import "./assets/scss/argon-dashboard-react.scss";
+import './assets/vendor/nucleo/css/nucleo.css';
+import './assets/vendor/@fortawesome/fontawesome-free/css/all.min.css';
+import './assets/scss/argon-dashboard-react.scss';
 import Login from './pages/LoginPage';
 import Create from './pages/CreatePage';
-
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
+      <Switch>
         <Route path="/login" component={Login} />
-        <Route path="/create" component={Create} />
-      <Redirect from="/" to="/admin/index" />
-    </Switch>
-  </BrowserRouter>
+        <Route path="/admin/create" component={Create}></Route>
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import routes from "../routes";
+import routes from '../routes';
 import Sidebar from '../components/Sidebar';
+import AdminNavbar from '../components/AdminNavbar';
+import Create from '../components/Create';
 
-export default class Create extends React.Component{
-    render() {
-        return (
-            <Sidebar
-            {...this.props}
-            routes={routes}
-            logo={{
-              innerLink: "/admin/index",
-              imgSrc: require("../assets/img/brand/argon-react.png"),
-              imgAlt: "..."
-            }}
-          /> 
-        )
-    }
+export default class CreatePage extends React.Component {
+  render() {
+    return (
+      <>
+        <Sidebar
+          {...this.props}
+          routes={routes}
+          logo={{
+            innerLink: '/admin/index',
+            imgSrc: require('../assets/img/brand/argon-react.png'),
+            imgAlt: '...'
+          }}
+        />
+        <div className="main-content" ref="mainContent">
+          <AdminNavbar brandText="Create Admin" />
+          <Create></Create>
+        </div>
+      </>
+    );
+  }
 }
